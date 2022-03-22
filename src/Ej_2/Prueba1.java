fb04944ecc4cd379e680d516e8250d1baf3f666b
Gorka9898 <glarranagaos20dw@ikzubirimanteo.com>
2022-03-22 09:50:22 +0100

    empezando

---
+++ src/Ej_2/Prueba1.java
@@ -0,0 +1,54 @@
+package Ej_2;
+
+public class Prueba1 {
+    int max;
+    int min;
+
+
+    public static void generate1num(int max, int min){
+
+        double a = min + (Math.random() * max);
+
+    System.out.print(a);
+
+    }
+
+    public static void generateNums(int max, int min, int amount){
+
+
+
+
+
+    }
+
+    public static void addNum(int num, String name){
+
+
+
+    }
+
+
+    public static void addNums(int num[], String name){
+
+
+
+    }
+    public static void findInFile(){
+
+
+
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+        Prueba1 n1=new Prueba1();
+
+        n1.generate1num(3,6);
+
+    }
+}
--- src/aaa/Prueba1.java
+++
@@ -1,7 +0,0 @@
-package aaa;
-
-public class Prueba1 {
-     public static void main(String[] args) {
-        System.out.print("AAAA");
-    }
-}
